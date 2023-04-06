88d62287b96138275ef90f0ec51cadd815afb72d
Corbin <corbin.johns.cj@gmail.com>
2023-04-06 19:41:15 -0400

    started work on User model

--- models/Thought.js
+++
@@ -1,7 +0,0 @@
-const { Schema, model } = require('mongoose');
-
-const userSchema = new Schema(
-    {
-        username: String,
-    }
-)
---
+++ models/User.js
@@ -0,0 +1,41 @@
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            trim: true,
+            required: true,
+            max_length: 20,
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            match: /.+\@.+\..+/,
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+);
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+module.exports = model('User', userSchema);
