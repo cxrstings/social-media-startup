ebc2eca53b9ebbe24df4523641ff458055d1e8c1
Corbin <corbin.johns.cj@gmail.com>
2023-04-06 20:56:22 -0400

    worked on routes and controllers and added to the index.js file

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,4 +1,52 @@
 const router = require('express').Router();
-// const { 
-    // controller stuff here 
-// } 
+const { 
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend
+} = require('../controllers/user-controller');
+
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    deleteReaction
+  } = require('../controllers/thought-controller');
+
+// Users routes
+router.route('/users')
+  .get(getAllUsers)
+  .post(createUser);
+
+router.route('/users/:id')
+  .get(getUserById)
+  .put(updateUser)
+  .delete(deleteUser);
+
+router.route('/users/:userId/friends/:friendId')
+  .post(addFriend)
+  .delete(removeFriend);
+
+// Thoughts routes
+router.route('/thoughts')
+  .get(getAllThoughts)
+  .post(createThought);
+
+router.route('/thoughts/:id')
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(deleteThought);
+
+router.route('/thoughts/:thoughtId/reactions')
+  .post(createReaction);
+
+router.route('/thoughts/:thoughtId/reactions/:reactionId')
+  .delete(deleteReaction);
+
+module.exports = router;
